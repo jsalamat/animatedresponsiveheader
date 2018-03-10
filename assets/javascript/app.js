3a424f7781af1086766d722a35d69e06299745bb
jsalamat <salamat_jon@yahoo.com>
2018-03-09 23:59:13 -0500

    app js working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,37 @@
-alert("Javascript and jQuery works!");
-console.log('you can erase this if alert works');
+(function() {
+
+  'use strict';
+
+  var main = document.querySelector("main"),
+      header = document.querySelector("header"),
+      hHeight;
+
+  function setTopHeight() {
+    hHeight = header.offsetHeight;
+    main.style.paddingTop = hHeight + "px";
+  }
+
+  function onScroll() {
+
+    window.addEventListener("scroll", callbackFunc);
+
+    function callbackFunc() {
+      var y = window.pageYOffset;
+      if (y > 50) {
+        header.classList.add("scroller");
+      } else {
+        header.classList.remove("scroller");
+      }
+    }
+  }
+
+  window.onload = function() {
+    setTopHeight();
+    onScroll();
+  };
+
+  window.onresize = function() {
+    setTopHeight();
+    onScroll();
+  };
+})();
